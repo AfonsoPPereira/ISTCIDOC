71d8fa21137e8ccfa69bd65bdf6a30f46a692b4a
AfonsoPPereira <49283619+AfonsoPPereira@users.noreply.github.com>
2019-05-02 01:49:08 +0100

    Add files via upload

---
+++ files/requires.php
@@ -0,0 +1,29 @@
+<?php
+
+//require 'vendor/autoload.php';
+
+/* File Name define (RDF/OWL) */
+define("FILENAME_RDF", "ist-crm.rdf");
+
+/* Generic Defines */
+define("TYPE", "rdf:type");
+define("LABEL", "rdfs:label");
+define("SCOPENOTE", "skos:scopeNote");
+define("RANGE", "rdfs:range");
+define("DOMAIN", "rdfs:domain");
+define("ABOUT", "rdf:about");
+define("COMMENT", "rdfs:comment");
+define("VALUE", "value:protected");
+
+/* Classes Define */
+define("CLASSES", "owl:Class");
+define("SUBCLASSOF", "rdfs:subClassOf");
+
+/* Properties Define */
+define("PROPERTIES", "owl:ObjectProperty");
+define("SUBPROPERTYOF", "rdfs:subPropertyOf");
+
+/* Ontology Name */
+define("ONTOLOGY", "istcidoc");
+
+?>
